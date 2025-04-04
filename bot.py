5b89b53d30ce461666e73be07eae80f1e6c63299
melitinie <148791626+melitinie@users.noreply.github.com>
2025-04-05 00:27:45 +0300

    Add files via upload
    Изменила файл по видеоуроку

--- bot.py
+++ bot.py
@@ -1,25 +1,21 @@
-from config_reader import config
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
 
-
-
-logging.basicConfig(level=logging.INFO)
+from aiogram import Bot, Dispatcher
+from config_reader import config
+from app.handlers import router
 
 bot = Bot(token=config.bot_token.get_secret_value())
-
 dp = Dispatcher()
 
 
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
-
-
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    logging.basicConfig(level=logging.INFO)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
