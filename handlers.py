9f3983efb58cb6fefadb8fec9d545dc5f6844b44
melitinie <148791626+melitinie@users.noreply.github.com>
2025-04-05 02:50:38 +0300

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -2,9 +2,16 @@ from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
+import app.keyboards as kb
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply("Здравствуйте! Это чат-бот канала «ФОРум». Здесь Вы можете записаться на сделку и изучить услуги банка. Напоминаем – нашу переписку видят только сотрудники банка и Вы.")
+    await message.answer("Здравствуйте! Это чат-бот канала «ФОРум». Здесь Вы можете записаться на сделку и изучить услуги банка. Напоминаем – нашу переписку видят только сотрудники банка и Вы.",
+                        reply_markup=kb.main)
+
+@router.message(F.text.lower() == "записаться на сделку")
+async def book_deal(message: Message):
+    await message.answer("Пожалуйста, выберите интересующую Вас категорию услуг:",
+                        reply_markup=kb.deal_category)
