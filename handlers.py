2e1005324b8490f6f58adfb8173a8da192cd17f3
melitinie <148791626+melitinie@users.noreply.github.com>
2025-04-05 03:14:20 +0300

    Add files via upload

--- handlers.py
+++ handlers.py
@@ -15,3 +15,13 @@ async def cmd_start(message: Message):
 async def book_deal(message: Message):
     await message.answer("Пожалуйста, выберите интересующую Вас категорию услуг:",
                         reply_markup=kb.deal_category)
+
+@router.message(F.text.lower() == "посмотреть услуги банка")
+async def view_services(message: Message):
+    await message.answer("Какие услуги Вас интересуют?",
+                         reply_markup=kb.other_services)
+
+@router.message(F.text.lower() == "задать вопрос")
+async def ask_question(message: Message):
+    await message.answer("Задайте Ваш вопрос и консультант ответит на него в ближайшее время")
+    #Не знаю как это реализовать. Может быть считать ID клиента и направить его в БД, а как консультант с ним свяжется это уже другая история
